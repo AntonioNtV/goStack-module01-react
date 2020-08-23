74316198628db8001317858ce56781bd722371c0
AntonioNtV <antoniontv18@gmail.com>
2020-08-23 12:25:16 -0300

    Adicao de estilizacao usando css

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
+import './App.css'
+import backgroundImage from './assets/background.jpg'
 /**
  * Principais conceitos do react
  * Componente
@@ -24,6 +26,7 @@ function App() {
     return (
         <> 
         <Header title="Projects"/>
+        <img width={300} src={backgroundImage}/>
 
         <ul>
             {projects.map(project => <li key={project}>{project}</li>)}
