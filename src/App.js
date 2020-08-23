7c761cbab12abbddfacfd82a8a9e52b9ae4e39d0
AntonioNtV <antoniontv18@gmail.com>
2020-08-23 11:56:13 -0300

    Simplificando o projeo

--- src/App.js
+++ src/App.js
@@ -4,25 +4,12 @@ import Header from './components/Header'
  * Principais conceitos do react
  * Componente
  * Propriedade
- * Estado
+ * Estado & Imutabilidade
  */
 function App() {
     return (
         <> 
-        <Header title="Homepage">
-            <ul>
-                <li>Homepage</li>
-                <li>Projects</li>
-            </ul>
-        </Header>
-        <Header title="Projects">
-            <ul>
-                <li>Homepage</li>
-                <li>Projects</li>
-                <li>Login</li>
-            </ul>
-        </Header>
-        <h1>Hello World</h1>
+        <Header title="Projects"/>
         </>
     )
 }
