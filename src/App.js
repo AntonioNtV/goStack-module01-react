9622ed4d4edd5d2ca577b93bed579ff8f770b6c0
AntonioNtV <antoniontv18@gmail.com>
2020-08-23 14:40:18 -0300

    Removendo imagem de fundo

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useState } from 'react'
 import Header from './components/Header'
 import './App.css'
-import backgroundImage from './assets/background.jpg'
 /**
  * Principais conceitos do react
  * Componente
@@ -26,7 +25,6 @@ function App() {
     return (
         <> 
         <Header title="Projects"/>
-        <img width={300} src={backgroundImage}/>
 
         <ul>
             {projects.map(project => <li key={project}>{project}</li>)}
